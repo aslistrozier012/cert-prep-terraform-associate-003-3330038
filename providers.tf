61b4b3ec34b3e2caeb76c1f19c858430706d2d38
Asli Strozier <Astrozier@Hirevue.com>
2024-07-12 12:13:00 -0400

    modified providers.tf

--- providers.tf
+++ providers.tf
@@ -2,20 +2,20 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">= 5.31.0"
+      version = ">=5.31.0"
     }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
-  profile = "Helen"
+  profile = "VScode"
   # Configuration options
 }
 
 provider "aws" {
   alias = "west"
   region = "us-west-1"
-  profile = "Helen"
+  profile = "VScode"
+  
 }
-
