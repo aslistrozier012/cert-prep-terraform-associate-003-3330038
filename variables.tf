7cd5d19dd8ebbca91e93aedc8fd31c57754017f6
Asli Strozier <Astrozier@Hirevue.com>
2024-07-18 16:37:54 -0400

    rds demo creation

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "rds-db-username" {
+  type = string
+  description = "username for rds-demo db instance"
+  default = "admin"
+}
+
+variable "rds-db-password" {
+  type = string
+  description = "username for rds-demo db instance"
+  default = "demordspass1"
+}
